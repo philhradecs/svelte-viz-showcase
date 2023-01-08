4a14b58367a7690b5688c1052437df39b12040c2
Philip Hradecsni <ph@webundsoehne.com>
2023-01-08 13:01:49 +0100

    improve transitions

--- src/components/IconLink.svelte
+++ src/components/IconLink.svelte
@@ -8,11 +8,19 @@
 </script>
 
 <a href={link}>
-	<div class="flex items-center gap-4 group hover:bg-cyan-300/80 px-2 py-1 rounded-sm flex-wrap">
+	<div
+		class="flex items-center gap-4 group hover:bg-cyan-300/80 px-2 py-1 rounded-sm flex-wrap transition-colors duration-75"
+	>
 		<div class="shrink-0">
-			<svelte:component this={icon} size={32} class="text-cyan-300/80 group-hover:text-black" />
+			<svelte:component
+				this={icon}
+				size={32}
+				class="text-cyan-300/80 group-hover:text-black transition-colors duration-75"
+			/>
 		</div>
-		<div class="tracking-wide text-xl font-bold group-hover:text-black">
+		<div
+			class="tracking-wide text-xl font-bold group-hover:text-black transition-colors duration-75"
+		>
 			{title}
 		</div>
 	</div>
