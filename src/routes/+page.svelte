412a801faa5fe52cb216319cfb787c8e180fc731
Philip Hradecsni <ph@webundsoehne.com>
2023-04-23 20:10:25 +0200

    fix typo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,10 +13,7 @@
 				Welcome to <span class="text-cyan-500">Viz</span>
 			</h1>
 			<p class="lg:text-lg text-justify lg:text-start font-mono">
-				D3.js is a JavaScript library for manipulating documents based on data. D3 helps you bring
-				data to life using HTML, SVG, and CSS. D3’s emphasis on web standards gives you the full
-				capabilities of modern browsers without tying yourself to a proprietary framework, combining
-				powerful showcaseization components and a data-driven approach to DOM manipulation.
+				D3.js is a JavaScript library for manipulating documents based on data. D3 helps you bring data to life using HTML, SVG, and CSS. D3's emphasis on web standards gives you the full capabilities of modern browsers without tying yourself to a proprietary framework, combining powerful visualization components and a data-driven approach to DOM manipulation.
 			</p>
 		</div>
 
